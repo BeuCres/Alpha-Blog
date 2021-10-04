c0557e3e8a2c038b6d922465b5681b580508696f
Beulah Cresel <beulah.cts@gmail.com>
2021-10-04 18:32:20 +0530

    Add boolean admin user field to app

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
     get 'about' , to: 'pages#about'
     get 'home' , to: 'pages#home'
     resources :articles
+    #get 'article', to: 'articles#destroy'
     get 'signup', to: 'users#new'
     resources :users, except: [:new]
+    #get 'user', to: 'users#destroy'
     get 'login', to: 'sessions#new'
     post 'login', to: 'sessions#create'
     get 'logout', to: 'sessions#destroy'
